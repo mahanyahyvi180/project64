c2923b1a251a02477d37819e2824f4c3e9fec9c1
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-09 17:40:21 +0330

    Add files via upload

---
+++ ConsoleApp64/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a Color:");
+
+            string color = Console.ReadLine();
+
+            Console.Write("enter a plural nun:");
+               string plural   = Console.ReadLine();
+
+            Console.Write("enter a celebrity :");
+            string celebrity = Console.ReadLine();
+
+
+            Console.WriteLine("roses are" + color);
+            Console.WriteLine(plural + "are blue");
+            Console.WriteLine("Ilove" + celebrity);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
